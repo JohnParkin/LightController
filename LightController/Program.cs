e7f96e9576c43745dd23756c5ca187eaf010e0ca
John Parkin <john.parkin@sharp-ls.eu>
2020-02-04 17:39:56 +0000

    Add project files.

---
+++ LightController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LightController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
